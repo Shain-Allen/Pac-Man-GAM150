c4e857a78342c5a80676ddce63066624691f4980
Shain-Allen <48696021+Shain-Allen@users.noreply.github.com>
2020-03-03 15:03:25 -0800

    h

---
+++ Working Game/Day3/Pac-Man/Assets/PacManAssets/Scripts/WallChecker.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallChecker : MonoBehaviour
+{
+    public enum Tiletype { illegal, legale };
+    public Tiletype[,] grid;
+    public float overlapCheckRadius = 1.0f;
+    public GameObject debugsquare;
+    public int width = 26;
+    public int height = 36;
+    public bool debugIt = false;
+    Vector2 startingGridCord = new Vector2(-13.5f, 15.5f);
+    Vector2 currentGridCord;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        grid = new Tiletype[width, height];
+        currentGridCord = startingGridCord;
+
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                if (Physics2D.OverlapCircle(currentGridCord, overlapCheckRadius))
+                {
+                    grid[i, j] = Tiletype.illegal;
+                    if (debugIt == true)
+                        Instantiate(debugsquare, currentGridCord, Quaternion.identity);
+                }
+                else
+                {
+                    grid[i, j] = Tiletype.legale;
+                }
+
+                currentGridCord.x += 1.0f;
+            }
+            currentGridCord.y += -1.0f;
+            currentGridCord.x = startingGridCord.x;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
